cf6c35a7133e04345ceba379a08a747953a4725b
Yesenia <yeseniamolinab@gmail.com>
2021-04-15 18:20:44 -0600

    feedback

--- session1/2-two-sum.js
+++ session1/2-two-sum.js
@@ -1,3 +1,4 @@
+// First try
 function twoSum(nums, target) {
     for(let i = 0; i < nums.length; i++) {
         for(let j = 0; j < nums.length; j++) {
@@ -13,9 +14,31 @@ function twoSum(nums, target) {
     return [];
 }
 
+// Second try after feedback
+function twoSumB(nums, target) {
+    let complementMap = new Map();
+    for(let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+        if(complementMap.has(complement)) {
+            return [complementMap.get(complement), i];
+        }
+        complementMap.set(nums[i], i);
+    }
+    return [];
+}
+
+/** First Try Tests */
 // Big O
-console.log('The time & space complexity: O(n ^ 2): - Quadratic time:', '\n')
+console.log('twoSum The time & space complexity: O(n ^ 2): - Quadratic time:', '\n')
 
 // Tests
 console.log(twoSum([2,7,1,5], 9), '[0,1]');
 console.log(twoSum([3,-1,0,1], 0), '[1,3]');
+
+/** Second Try Tests */
+// Big O
+console.log('twoSumB The time & space complexity: O(n): - Linear time:', '\n')
+
+// Tests
+console.log(twoSumB([2,7,1,5], 9), '[0,1]');
+console.log(twoSumB([3,-1,0,1], 0), '[1,3]');
