dae11613c171dac98136e5eed4a5f626247b022e
Yesenia <yeseniamolinab@gmail.com>
2021-05-03 09:20:53 -0600

    parentheses using stacks

--- session3/parentheses.js
+++ session3/parentheses.js
@@ -1,3 +1,5 @@
+import { Stack } from "../utils/stack.js";
+
 function minValidParentheses(input) {
     let res = 0;
     let balance = 0;
@@ -13,9 +15,36 @@ function minValidParentheses(input) {
     return res + balance;
 }
 
+function minValidParenthesesWithStack(input) {
+    const mappings = new Map();
+    mappings.set(')', '(');
+    mappings.set('}', '{');
+    mappings.set(']', '[');
+    
+    let stack = new Stack();
+    let leftover = 0;
+    
+    for(let i = 0; i < input.length; i++) {
+        const parentheses = input[i];
+        if(mappings.has(parentheses)) {
+            const top = stack.empty() ? '' : stack.pop();
+            if(top !== mappings.get(parentheses)) leftover++;
+        } else {
+            stack.push(parentheses);
+        }
+    }
+
+    return stack.size + leftover;
+}
+
 console.log("Time & Space(? not sure about space complexity) complexity is Linear O(N)");
 
 console.log('())', minValidParentheses('())'));
 console.log('(((', minValidParentheses('((('));
 console.log('()', minValidParentheses('()'));
 console.log('()))((', minValidParentheses('()))(('));
+console.log('**************************************');
+console.log('())', minValidParenthesesWithStack('())'));
+console.log('(((', minValidParenthesesWithStack('((('));
+console.log('()', minValidParenthesesWithStack('()'));
+console.log('()))((', minValidParenthesesWithStack('()))(('));
